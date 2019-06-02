c5646b6c9be3b3f7cfcde036bbe60835509c0ec9
jhordanSnow <jhordan.gt@gmail.com>
2019-06-01 18:21:56 -0600

    mostrar agentes y servicios

--- web/js/Algorithm.js
+++ web/js/Algorithm.js
@@ -2,29 +2,45 @@ let agents;
 let services;
 
 function showAgents(){
-    console.log('Agents', agents)
+    jQuery.each(agents.agent, function (i, agent){
+        var htmlAgent = "<tr>"+
+        "<td>" + agent.id +"</td>" +
+        "<td>" + agent.name +"</td>" +
+        "<td>";
+        htmlAgent += agent.services.id;
+        htmlAgent += "</td>" +
+        "</tr>";
+        jQuery("#AgentsTarget").append(htmlAgent);
+    });
 }
 
 function showServices(){
-    console.log("Services", services)
+    console.log(services)
+    jQuery.each(services.service, function (i, service){
+        var htmlAgent = "<tr>"+
+        "<td>" + service.id +"</td>" +
+        "<td>" + service.code +"</td>" +
+        "<td>" + service.client + "</td>" +
+        "</tr>";
+        jQuery("#ServicesTarget").append(htmlAgent);
+    });
 }
 
 window.onload = function() {
     jQuery("#uploadAgents").change(function (e){
         if (e.target.files.length <= 0) return false;
-        const file = e.target.files[0];
-        readUploadedFileAsText(file).then((value) =>{
-            agents = value.agents;
+
+        readUploadedFileAsText(e.target.files[0]).then((value) =>{
+            agents = value;
             showAgents();
         });
     });
 
     jQuery("#uploadServices").change(function (e){
         if (e.target.files.length <= 0) return false;
-        const file = e.target.files[0];
-        readUploadedFileAsText(file).then((value) =>{
-            console.log(value);
-            services = value.services;
+        
+        readUploadedFileAsText(e.target.files[0]).then((value) =>{
+            services = value;
             showServices();
         });
     });
