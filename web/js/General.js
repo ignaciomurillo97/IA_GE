aeeb145dd40991bb6e88c7c7d18f525880479802
Ignacio Murillo <ignaciomurillo97@gmail.com>
2019-06-02 00:05:36 +0000

    funcion de jhordan

--- web/js/General.js
+++ web/js/General.js
@@ -78,3 +78,56 @@ function xmlToJson(xml) {
   });
 
 })(jQuery); // End of use strict
+
+function xmlToJson(xml) {
+    var objeto = {};
+    var esRaiz = false;
+
+    if (xml.nodeType == 1) {
+        if (xml.attributes.length > 0) {
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var atributo = xml.attributes.item(j);
+                objeto[atributo.nodeName] = atributo.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) {
+        objeto = xml.nodeValue;
+    } else if (xml.nodeType == 9) {
+        esRaiz = true;
+    }
+
+    if (xml.hasChildNodes()) {
+        for(var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nombreNodo = item.nodeName;
+
+            if (typeof(objeto[nombreNodo]) == "undefined") {
+                if (nombreNodo == "#cdata-section") {
+                    objeto = item.nodeValue;
+                } else if (nombreNodo == "#text") {
+                    if (item.childNodes.length < 1) {
+                        objeto = item.nodeValue;
+                    } else {
+                        objeto[nombreNodo] = xmlToJson(item);
+                    }
+                } else {
+                    if (esRaiz) {
+                        objeto = xmlToJson(item);
+                    } else {
+                        objeto[nombreNodo] = xmlToJson(item);
+                    }
+                }
+            } else {
+                if (typeof(objeto[nombreNodo].push) == "undefined") {
+                    var valorAtributo = objeto[nombreNodo];
+                    objeto[nombreNodo] = new Array();
+                    objeto[nombreNodo].push(valorAtributo);
+                }
+
+                objeto[nombreNodo].push(xmlToJson(item));
+            }
+        }
+    }
+    
+    return objeto;
+};
